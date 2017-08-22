c985585b67daa4f3b81aec60928961c34c1e6970
André Marcelino <andrercm@br.ibm.com>
2017-08-22 10:24:44 -0300

    adding simple automated test

---
+++ tests/scripts/basicTest.js
@@ -0,0 +1,23 @@
+module.exports = {
+  // 'Demo test GM' : function (browser) {
+  //   browser
+  //     .url('https://ibmmethod-beta.mybluemix.net/devops/method')
+  //     .waitForElementVisible('.searchbox.hidden-xs.hidden-sm', 60000, true,
+  //       function(){}, 'Waiting for Search Input to become visible')
+  //     .setValue('.searchbox.hidden-xs.hidden-sm', 'Test')
+  //     .waitForElementPresent('#search-page', 60000)
+  //     .pause(5000)
+  //     .end();
+  // }
+  'Demo test for Gift Back' : function (browser) {
+    browser
+      .url('http://giftback-test.mybluemix.net')
+      .waitForElementVisible('.container-fluid', 60000, true,
+        function(){}, 'Waiting for page to load')
+      .waitForElementPresent('#givefeedback', 60000)
+      .onClick('#givefeedback')
+      .waitForElementVisible('.media-object') //check if class that contains image will display
+      .pause(1000)
+      .end();
+  }
+};
